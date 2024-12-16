05745dfab9f246410b3cc51701dfb681d723239f
adrianchinjen <adrian.chinjen28@gmail.com>
2024-12-16 13:16:08 +0800

    feat: educational background

---
+++ src/app/portfolio/components/pages/EducationalBackground.tsx
@@ -0,0 +1,11 @@
+const EducationalBackground = () => {
+  return (
+    <>
+      <div className="mb-20 mt-40 flex justify-center text-5xl font-bold drop-shadow-md">
+        Educational Background
+      </div>
+    </>
+  );
+};
+
+export default EducationalBackground;
