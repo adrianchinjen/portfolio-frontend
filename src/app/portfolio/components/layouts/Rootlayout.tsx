4cf150b7454dd874b63415f2cfa7e4fd12f9cd0c
adrianchinjen <adrian.chinjen28@gmail.com>
2024-12-18 14:08:47 +0800

    feat: hero animations done and add lazy load for hero section

--- src/app/portfolio/components/layouts/Rootlayout.tsx
+++ src/app/portfolio/components/layouts/Rootlayout.tsx
@@ -1,10 +1,10 @@
-import Navbar from './Navbar';
-import HeroSection from '../pages/HeroSection';
-import ExperienceSection from '../pages/ExperienceSection';
-import SkillsSection from '../pages/SkillsSection';
-// import CertificationSection from '../pages/CertificationSection';
-import Footer from '../pages/Footer';
-import { useEffect, useState } from 'react';
+import { lazy, Suspense, useEffect, useState } from 'react';
+
+const NavBar = lazy(() => import('./Navbar'));
+const HeroSection = lazy(() => import('../pages/HeroSection'));
+const ExperienceSection = lazy(() => import('../pages/ExperienceSection'));
+const SkillsSection = lazy(() => import('../pages/SkillsSection'));
+const Footer = lazy(() => import('../pages/Footer'));
 
 const RootLayout = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -15,31 +15,33 @@ const RootLayout = () => {
   }, []);
   return (
     <div>
-      <div className="relative z-50">
-        <Navbar />
-      </div>
-      {isVisible ? (
-        <>
-          <div className="max-w-8xl mx-auto min-h-91 px-6 pt-10 3xl:min-h-93">
-            <HeroSection />
-            <ExperienceSection />
-            <SkillsSection />
-            {/* <CertificationSection /> */}
-          </div>
-          <div className="max-w-8xl mx-auto mt-20 bg-zinc-800 px-6 dark:bg-black">
-            <Footer />
-          </div>
-          <footer className="flex w-full flex-row justify-center gap-10 border-t border-zinc-700 bg-zinc-800 px-5 py-1 text-gray-400 dark:bg-black">
-            <p className="text-sm text-df-light">
-              © {2023} – {new Date().getFullYear()}
-            </p>
-            <p className="text-sm text-df-light">A | Chinjen.</p>
-            <p className="text-sm text-df-light">All rights reserved.</p>
-          </footer>
-        </>
-      ) : (
-        <>Loading</>
-      )}
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="relative z-50">
+          <NavBar />
+        </div>
+        {isVisible ? (
+          <>
+            <div className="max-w-8xl mx-auto min-h-91 px-6 pt-10 3xl:min-h-93">
+              <HeroSection />
+              <ExperienceSection />
+              <SkillsSection />
+              {/* <CertificationSection /> */}
+            </div>
+            <div className="max-w-8xl mx-auto mt-20 bg-zinc-800 px-6 dark:bg-black">
+              <Footer />
+            </div>
+            <footer className="flex w-full flex-row justify-center gap-10 border-t border-zinc-700 bg-zinc-800 px-5 py-1 text-gray-400 dark:bg-black">
+              <p className="text-sm text-df-light">
+                © {2023} – {new Date().getFullYear()}
+              </p>
+              <p className="text-sm text-df-light">A | Chinjen.</p>
+              <p className="text-sm text-df-light">All rights reserved.</p>
+            </footer>
+          </>
+        ) : (
+          <>Loading</>
+        )}
+      </Suspense>
     </div>
   );
 };
