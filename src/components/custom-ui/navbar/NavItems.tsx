75f130c63dff65ef69281ff6386591de8f177945
adrianchinjen <adrian.chinjen28@gmail.com>
2024-12-19 20:19:43 +0800

    feat: done all out navbar enhancement

--- src/components/custom-ui/navbar/NavItems.tsx
+++ src/components/custom-ui/navbar/NavItems.tsx
@@ -12,17 +12,17 @@ const NavItems = ({ isOpen, onNavigate, sectionRefs }: NavListProps) => {
     <>
       {/* BROWSER NAV ITEMS */}
       <ul className="hidden items-center gap-12 text-base font-semibold xl:flex">
-        <li className="cursor-pointer rounded-md p-3 transition-all hover:bg-sky-400 hover:text-white">
+        <li className="dark:hover:text-yellow cursor-pointer rounded-md p-3 transition-all hover:text-df-yellow">
           <NavLink to="" onClick={() => onNavigate(sectionRefs.heroRef)}>
             Home
           </NavLink>
         </li>
-        <li className="cursor-pointer rounded-md p-3 transition-all hover:bg-sky-400 hover:text-white">
+        <li className="dark:hover:text-yellow cursor-pointer rounded-md p-3 transition-all hover:text-df-yellow">
           <NavLink to="" onClick={() => onNavigate(sectionRefs.experienceRef)}>
             Experience
           </NavLink>
         </li>
-        <li className="cursor-pointer rounded-md p-3 transition-all hover:bg-sky-400 hover:text-white">
+        <li className="dark:hover:text-yellow cursor-pointer rounded-md p-3 transition-all hover:text-df-yellow">
           <NavLink to="" onClick={() => onNavigate(sectionRefs.skillsRef)}>
             Skills
           </NavLink>
@@ -34,17 +34,20 @@ const NavItems = ({ isOpen, onNavigate, sectionRefs }: NavListProps) => {
         className={`absolute left-0 top-20 flex w-full transform flex-col items-center gap-6 bg-df-light text-lg font-semibold transition-transform dark:bg-zinc-800 xl:hidden ${isOpen ? 'opacity-100' : 'opacity-0'}`}
         style={{ transition: 'transform 0.3s ease, opacity 0.3s ease' }}
       >
-        <li className="w-full cursor-pointer list-none p-4 text-center transition-all hover:bg-sky-400 hover:text-white">
-          Home
+        <li className="dark:hover:text-yellow w-full cursor-pointer list-none p-4 text-center transition-all hover:text-df-yellow">
+          <NavLink to="" onClick={() => onNavigate(sectionRefs.heroRef)}>
+            Home
+          </NavLink>
         </li>
-        <li className="w-full cursor-pointer list-none p-4 text-center transition-all hover:bg-sky-400 hover:text-white">
-          Products
+        <li className="dark:hover:text-yellow w-full cursor-pointer list-none p-4 text-center transition-all hover:text-df-yellow">
+          <NavLink to="" onClick={() => onNavigate(sectionRefs.experienceRef)}>
+            Experience
+          </NavLink>
         </li>
-        <li className="w-full cursor-pointer list-none p-4 text-center transition-all hover:bg-sky-400 hover:text-white">
-          Explore
-        </li>
-        <li className="w-full cursor-pointer list-none p-4 text-center transition-all hover:bg-sky-400 hover:text-white">
-          Contact
+        <li className="dark:hover:text-yellow w-full cursor-pointer list-none p-4 text-center transition-all hover:text-df-yellow">
+          <NavLink to="" onClick={() => onNavigate(sectionRefs.skillsRef)}>
+            Skills
+          </NavLink>
         </li>
       </ul>
     </>
